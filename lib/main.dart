6ace4a2d7219ebcf42ce7612a473ec13866a6c8e
Mexl <mexldelvertuba@gmail.com>
2024-02-20 21:45:46 +0800

    directories and font setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,110 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pet Store',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PetStoreHomePage(),
+    );
+  }
+}
+
+class PetStoreHomePage extends StatelessWidget {
+  final List<Map<String, dynamic>> pets = [
+    {'name': 'Persian Cat', 'price': '\$85', 'image': 'Persian.png'},
+    {'name': 'Sphinx Cat', 'price': '\$60', 'image': 'Sphinx.png'},
+    {'name': 'Bengal Cat', 'price': '\$90', 'image': 'Bengal.png'},
+    {'name': 'Abyssinian Cat', 'price': '\$75', 'image': 'Abyssinian.png'},
+    {'name': 'Burmese Cat', 'price': '\$75', 'image': 'Burmese.png'},
+    {'name': 'Russian Blue Cat', 'price': '\$60', 'image': 'Russian.png'},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Store'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search Product or Brand',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(8),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.8,
+              ),
+              itemCount: pets.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 2,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/${pets[index]['image']}',
+                        fit: BoxFit.cover,
+                        height: 120, // Adjust the size to fit your layout
+                      ),
+                      Text(pets[index]['name']),
+                      Text(pets[index]['price'],
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.view_list),
+            label: 'Catalog',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
